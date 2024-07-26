e18a8d9f21d63392ea4ea6a0c1ba792d087ee5db
TheEndBoss101 <theendboss101@gmail.com>
2024-07-26 14:32:43 -0500

    set default options

--- app/View/Composers/App.php
+++ app/View/Composers/App.php
@@ -31,13 +31,13 @@ class App extends Composer
                 'enabled' => true, // Whether the header is enabled
                 'advanced_ProjectNameAndLogo' => true, // Show an advanced project name and logo in the header
                 'UseDarkHeader' => true, // Use a dark theme for the header
-                'extended' => false,
-                'megamenu' => true, // TODO: Implement support for a megamenu
+                'extended' => true,
+                'megamenu' => false, // TODO: Implement support for a megamenu
             ],
             // Footer settings
             'footer' => [
                 'enabled' => true, // Whether the footer is enabled
-                'size' => 'slim', // The size of the footer: 'big', 'med', or 'slim'. // TODO: Improve support for 'big' size
+                'size' => 'med', // The size of the footer: 'big', 'med', or 'slim'. // TODO: Improve support for 'big' size
             ],
             // Identifier settings
             'identifier' => [
